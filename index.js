1afa9f38a7fc70cd28ead5645c06311b1b403a40
Kim Green <kimgreen.writings@gmail.com>
2022-02-26 16:42:00 +0200

    commit tree d5f5bfc4f2fc2f4ebf926e60381e9d054bcc074e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+// writeCards()
+// 1) returns an array of thank you messages
+// for each name provided to the function
+function writeCards(names) {
+    return names.map((name) => `Thank you, ${name}, for the wonderful surprise gift!`);
+}
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+
+
+    //   1) invokes console.log 
+    //   once for each number, 
+    //   counting down from the number provided to zero
